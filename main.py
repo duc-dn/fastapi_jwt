d09e751113a08ecdea878e308ba29afbfc87f33d
ducdn <ducdn@vnpt.vn>
2023-11-20 14:49:26 +0700

    demo fastapi with jwt

---
+++ main.py
@@ -0,0 +1,56 @@
+# File main.py
+
+import uvicorn
+import jwt
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from typing import Union, Any
+from datetime import datetime, timedelta
+from fastapi.security import HTTPBearer
+from security import validate_token, reusable_oauth2
+
+SECURITY_ALGORITHM = 'HS256'
+SECRET_KEY = '123456'
+
+app = FastAPI(
+    title='FastAPI uJWT', openapi_url='/openapi.json', docs_url='/docs',
+    description='fastapi jwt'
+)
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+def verify_password(username, password):
+    if username == 'admin' and password == 'admin':
+        return True
+    return False
+
+def generate_token(username: Union[str, Any]) -> str:
+    expire = datetime.utcnow() + timedelta(
+        seconds= 60 * 60 * 24 * 3
+    )
+
+    to_encode = {
+        "exp": expire, "username": username
+    }
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=SECURITY_ALGORITHM)
+    return encoded_jwt
+
+@app.post('/login')
+def login(request_data: LoginRequest):
+    print(f'[x] request_data: {request_data.__dict__}')
+    if verify_password(username=request_data.username, password=request_data.password):
+        token = generate_token(request_data.username)
+        return {
+            'token': token
+        }
+    else:
+        return HTTPException(status_code=404, detail="user not found")
+
+@app.get('/books', dependencies=[Depends(validate_token)])
+def list_books():
+    return {'data': ['Sherlock Homes', 'Harry Potter', 'Rich Dad Poor Dad']}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
